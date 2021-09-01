7d9fe947a6a67f5e875d33d9fde7c725c71be480
Jonathan S. Medenilla <jonathanmedenilla.jonmed@gmail.com>
2021-09-01 18:19:04 +0800

    Update home.blade.php - Jonathan

--- routes/web.php
+++ routes/web.php
@@ -14,24 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Public Routes
-Route::get('/', [App\Http\Controllers\AppController::class, 'index'])->name('home');
 
-Route::middleware(['guest'])->group(function () {
-  Route::get('login', [App\Http\Controllers\LoginController::class, 'index'])->name('login');
-  Route::post('login', [App\Http\Controllers\LoginController::class, 'logIn'])->name('post.login'); 
-  Route::get('register', [App\Http\Controllers\RegisterController::class, 'index'])->name('register');
-  Route::post('register', [App\Http\Controllers\RegisterController::class, 'register'])->name('post.register');
-});
-Route::post('logout', [App\Http\Controllers\LoginController::class, 'logOut'])->name('logout');
-
-// Athenticated Routes
-Route::middleware(['auth'])->group(function () {
-  Route::get('dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard'); 
-  Route::group(['prefix' => 'account'], function () {
-      Route::get('/', [App\Http\Controllers\AccountController::class, 'index'])->name('account');
-      Route::post('update', [App\Http\Controllers\AccountController::class, 'updateAccount'])->name('account.update'); 
-  });
-});
+Route::get('/', [App\Http\Controllers\AppController::class, 'lexweb.home'])->name('home');
 
 
 // Carl Note
