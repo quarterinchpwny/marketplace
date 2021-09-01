9537b5ffdc3c1076b5e784990bbf05719fe21179
Gavin Dane Santos <danesnts@gmail.com>
2021-09-01 21:09:08 +0800

    initial layout for home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,21 +11,26 @@
   <!-- Fonts -->
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Archivo&display=swap" rel="stylesheet">
   <!-- Styles -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 <body>
   <!-- Header and Navbar -->
   <div class="container-fluid" id="topContent">
         <img src="/images/headerLogo.svg" alt="Rizal Logo" id="headerLogo">
     </div>
+  
   <div id="app">
      @include('layouts.nav', ['from' => 'app'])
-    <main class="py-4">
-      
-    </main>
+
   </div>
+  <main class="py-0">
+        @yield('testing')
+      </main>
  
-  @yield('testing')
 </body>
 </html>
