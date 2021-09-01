e4cfcea4f934ed8ebd771cb85d1bea619d939755
Jonathan S. Medenilla <jonathanmedenilla.jonmed@gmail.com>
2021-09-01 19:08:36 +0800

    8 - Update home.blade.php - Jonathan

--- resources/views/designs/design.blade.php
+++ resources/views/designs/design.blade.php
@@ -9,6 +9,34 @@
     <title>Lexweb Neutral</title>
 </head>
 <body>
+    <nav class="navbar nagbar-expand-lg navbar-light bg-ntlightcream p-6 justify-between">
+        <div class="container-fluid">
+            <ul class="nav justify-content-end"> 
+                <li class="nav-item"> 
+                    <a href="#" class="p-4"> Home </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="p-4"> Profile </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="p-4"> Our Services </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="p-4"> Law Update </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="p-4"> Law Practice </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="p-4"> Everyday Law </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="p-4"> Blog </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     @yield('content')
+    @yield('index')
 </body>
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,30 @@
 @extends('designs.design')
-
 @section('content')
-    Hello
+    <nav class="navbar nagbar-expand-lg navbar-light bg-ntlightcream p-6 justify-between">
+        <div class="container-fluid">
+            <ul class="nav justify-content-end"> 
+                <li class="nav-item"> 
+                    <a href="#" class="p-4"> Home </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="p-4"> Profile </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="p-4"> Our Services </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="p-4"> Law Update </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="p-4"> Law Practice </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="p-4"> Everyday Law </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="p-4"> Blog </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 @endsection
