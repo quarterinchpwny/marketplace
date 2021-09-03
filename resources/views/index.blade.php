95d6b5dbf602e91ef2abffd5b04be2343e0d9682
Jonathan S. Medenilla <jonathanmedenilla.jonmed@gmail.com>
2021-09-03 10:49:28 +0800

    Initial Commit - Jonathan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,30 +1,4 @@
 @extends('designs.design')
 @section('content')
-    <nav class="navbar nagbar-expand-lg navbar-light bg-ntlightcream p-6 justify-between">
-        <div class="container-fluid">
-            <ul class="nav justify-content-end"> 
-                <li class="nav-item"> 
-                    <a href="#" class="p-4"> Home </a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="p-4"> Profile </a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="p-4"> Our Services </a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="p-4"> Law Update </a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="p-4"> Law Practice </a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="p-4"> Everyday Law </a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="p-4"> Blog </a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <a href="{{ route('Lunod', ['name'  => 'Sample']) }}"> Hehe </a>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,24 +15,30 @@ use Illuminate\Support\Facades\Route;
 
 // Public Routes
 Route::get('/content', [App\Http\Controllers\AppController::class, 'index'])->name('content');
+    
+Route::middleware(['guest'])->group(function () {
+  Route::get('login', [App\Http\Controllers\LoginController::class, 'index'])->name('login');
+  Route::post('login', [App\Http\Controllers\LoginController::class, 'logIn'])->name('post.login'); 
+  Route::get('register', [App\Http\Controllers\RegisterController::class, 'index'])->name('register');
+  Route::post('register', [App\Http\Controllers\RegisterController::class, 'register'])->name('post.register');
+});
+Route::post('logout', [App\Http\Controllers\LoginController::class, 'logOut'])->name('logout');
 
-// Route::middleware(['guest'])->group(function () {
-//   Route::get('login', [App\Http\Controllers\LoginController::class, 'index'])->name('login');
-//   Route::post('login', [App\Http\Controllers\LoginController::class, 'logIn'])->name('post.login'); 
-//   Route::get('register', [App\Http\Controllers\RegisterController::class, 'index'])->name('register');
-//   Route::post('register', [App\Http\Controllers\RegisterController::class, 'register'])->name('post.register');
-// });
-// Route::post('logout', [App\Http\Controllers\LoginController::class, 'logOut'])->name('logout');
+// Athenticated Routes
+Route::middleware(['auth'])->group(function () {
+  Route::get('dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard'); 
+  Route::group(['prefix' => 'account'], function () {
+      Route::get('/', [App\Http\Controllers\AccountController::class, 'index'])->name('account');
+      Route::post('update', [App\Http\Controllers\AccountController::class, 'updateAccount'])->name('account.update'); 
+  });
+});
 
-// // Athenticated Routes
-// Route::middleware(['auth'])->group(function () {
-//   Route::get('dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard'); 
-//   Route::group(['prefix' => 'account'], function () {
-//       Route::get('/', [App\Http\Controllers\AccountController::class, 'index'])->name('account');
-//       Route::post('update', [App\Http\Controllers\AccountController::class, 'updateAccount'])->name('account.update'); 
-//   });
-// });
-
+Route::group(['prefix' => 'admin'], function (){
+    Route::get('test/{name}', [App\Http\Controllers\DashboardController::class, 'test'])->name('Lunod');
+    Route::get('/Hehe', function() {
+        dd('Stream Pebble House by Ben&Ben HEHE');
+    });
+});
 
 // Carl Note
 // if route changes not work try to run "php artisan optimize"
