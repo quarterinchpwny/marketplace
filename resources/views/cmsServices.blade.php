0e2b6670c1afb8c97a9406e4e178d60520f72d4b
Edmond Valdez <edmondvaldez11@gmail.com>
2021-09-13 18:17:27 +0800

    bg and buttons for some sections

--- resources/views/cmsServices.blade.php
+++ resources/views/cmsServices.blade.php
@@ -1,3 +1,13 @@
 @extends('layouts.cmsnavigation')
-@section('content')         
+@section('content')
+<div class="row">
+        <div class="col-10">
+            <p class="fs-5 fw-bold text-white">Everyday Law</p> 
+        </div>
+        <div class="col-1">
+          <button type="button" class="btn text-white addnewbtn" style = "background-color: #9D71BC;"> Preview</button>       
+        </div>
+</div>
+<div class="row2 container-fluid"  style="height: 580px; background-color:#F8FFFE; ">  
+</div>            
 @endsection
