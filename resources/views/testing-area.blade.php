4ffce969cec8a839e09a43278ce111efec8014b7
Julianne Nicole Geda <juliannegeda@gmail.com>
2021-09-13 11:31:51 +0800

    added testing-area.blade.php

---
+++ resources/views/testing-area.blade.php
@@ -0,0 +1,13 @@
+<div class="container">
+    @if (session('success'))
+      <div class="alert alert-success" role="alert">
+        {{ session('success') }}
+      </div>
+    @endif
+</div>
+
+@include('feminine-layouts.header')
+@include('feminine-layouts.banner-header')
+@include('feminine-layouts.practice-areas')
+@include('feminine-layouts.law-updates')
+@include('feminine-layouts.footer')
