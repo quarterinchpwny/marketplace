cd66de553a30aee0ecc5860b7bfdfcd1c361e8fb
Jonathan S. Medenilla <jonathanmedenilla.jonmed@gmail.com>
2021-09-13 17:21:48 +0800

    Update Neutral - Jonathan

--- resources/views/lexcontent/responsiveeverydaylaw.blade.php
+++ resources/views/lexcontent/responsiveeverydaylaw.blade.php
@@ -1,25 +1,25 @@
 <section id="everyday-law ">
-   <div id="everyday-law-section" style="background-color: white">
+   <div id="everyday-law-section" style="background-color: gray">
       <div class="row" id="everyday-law-header">
-               <h2 class="display-2 d-none d-sm-block"> Everyday Law  </h2>
-               <h2 class="display-2 d-block d-md-none text-center"> Everyday Law </h2>
-               <hr class="d-none d-sm-block" style="margin-left: 9.3%; margin-bottom: 2%; width: 15%;">
+         <h2 class="display-2 d-none d-sm-block">Everyday Law</h2>
+         <h2 class="display-2 d-block d-md-none text-center">Everyday Law</h2>
+         <hr class="d-none d-sm-block" style="margin-left: 9.3%; margin-bottom: 2%; width: 15%;">
       </div>
       <div class="columns image" id="everyday-law-media">
-         <img id="everyday-law-image"src="{{ URL::to('/') }}/NeutralHome_Graphics/law_video.png" alt="Law Updates">
+         <img id="everyday-law-image" src="{{ URL::to('/') }}/NeutralHome_Graphics/law_video.png" alt="Law Updates">
          &nbsp;
       </div>
       <div class="columns content">
          <div id="everyday-law-content">
-            <h2 class = "display-4 " id="everyday-law-title1">"Co-maker ng di nagbayad sa utang,</h2>
-            <h2 class = "display-4 " id="everyday-law-title2">Tama ba na ikaw ang singilin?"</h2>
+            <h2 class = "display-4" id="everyday-law-title1">"Co-maker ng di nagbayad sa utang,</h2>
+            <h2 class = "display-4" id="everyday-law-title2">Tama ba na ikaw ang singilin?"</h2>
             &nbsp;
             <p class = "display-7">
                Nakapirma ka na ba sa loan agreement bilang co-maker?
                <br>
                Ano ang panganib kung ikaw ay pipirma bilang co-maker sa isang loan?
             </p>
-            <div id="law-updates-button" style="padding: 5% 0 0 15%">
+            <div id="law-updates-button" style="padding: 0 0 0 15%">
                <a class="btn" href="#" role="button" id="areas_seemore">SEE MORE</a>
             </div>
          </div>
