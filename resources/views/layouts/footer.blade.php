31074049bb00190516de9feca9967304d61fe5c4
John Cedrick Alcantara <cedie.alcantara@gmail.com>
2021-09-15 16:43:48 +0800

    footer done

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 
-<div class="text-center text-lg-start text-light pt-0" style="background-color: #092059;">
+<footer class="ftr text-center text-lg-start text-light pt-0"">
 
             <!-- Section: Links  -->
             <section class="footerbody">
@@ -9,7 +9,7 @@
                     <div class="row contentsFooter">
                         <!-- Grid column -->
 
-                            <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
                              <!-- Content -->
                                 <img src="/images/rizalLogo.png" alt="Rizal Logo" id="logoFooter">
                                 <p class="text-center">Lorem ipsum dolor sit amet</p>
@@ -17,9 +17,9 @@
                             <!-- Grid column -->
 
                             <!-- Grid column -->
-                            <div class="col-xs-6 col-md-3 col-lg-3 col-xl-3 mx-auto mb-4 spacing">
+                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 mx-auto mb-4 spacing">
                              <!-- Links -->
-                                <h6 class="text-uppercase fw-bold mb-4 spacing" id="footerHeading">
+                                <h6 class="text-left text-uppercase fw-bold mb-4 spacing" id="footerHeading">
                                     Visit
                                 </h6>
                                 <p class="text-left" id="footerInfo">
@@ -32,9 +32,9 @@
                             <!-- Grid column -->
 
                             <!-- Grid column -->
-                            <div class="col-xs-6 col-md-6 col-lg-3 col-xl-2 mb-4 spacing">
+                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 mb-4 spacing">
                              <!-- Links -->
-                                <h6 class="text-uppercase fw-bold mb-4 spacing" id="footerHeading">
+                                <h6 class="text-left text-uppercase fw-bold mb-4 spacing" id="footerHeading">
                                     Contact
                                 </h6>
                                 <p class="text-left" id="footerInfo" >
@@ -45,12 +45,12 @@
                             <!-- Grid column -->
 
                             <!-- Grid column -->
-                            <div class="col-xs-6 col-md-3 col-lg-4 col-xl-3 mx-auto mb-md-0 mb-4 spacing">
+                            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 pb-3">
                                 <!-- Links -->
-                                    <h6 class="text-uppercase fw-bold mb-4 spacing" id="footerHeading">
+                                    <h6 class="text-left text-uppercase fw-bold mb-4 spacing" id="footerHeading">
                                         Socials
                                     </h6>
-                                <div>
+                                <div class="logocenter">
                                     <a href="#" class="me-3 text-reset">
                                         <img src="/images/fbLogo.svg" alt="facebook logo" height=25px width=25px>
                                     </a>
@@ -74,63 +74,37 @@
 
                 <!-- Grid row -->
                 </div>
+
+
+
             </section>
-  <!-- Section: Links  -->
-
-  <!-- Copyright -->
-
-
-
-        {{-- <div class="container text-center text-md-start mt-2">
-                <p class="text-left poweredBy">Powered by <img src="/images/lexmeetLogo.png"  alt="lexlogo" height="13" width="85"></p>
-
-                    <div class="text-right p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-                                <ul>
-                                    <a href="" id="linksLegal">Terms of Use</a>
-                                    <a href="" id="linksLegal">Privacy Policy</a>
-                                    <a href="" id="linksLegal">Informed Consent of Service Performed</a>
-                                    <a href="" id="linksLegal">Disclaimer</a>
-                                </ul>
-                    </div>
-        </div> --}}
-  <!-- Copyright -->
-</div>
-
 <hr>
+  <div class="container text-md-start">
+        <div class="row">
 
-<div class="container" style="background-color: #092059;">
-    <div class="row footerrow">
-        <div class="col-md-8">
-            <div class="nav">
-                <div class="nav-item">
-                    <a href="#" class="p-2 nav-link">
-                        <p id="linksLegal">Terms of Use</p>
-                    </a>
+                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                    <p class="text-left text-primary">Powered by <img src="/images/lexmeetLogo.png"  alt="lexlogo" height="13" width="85"></p>
                 </div>
 
-                <div class="nav-item">
-                    <a href="#" class="p-2 nav-link">
-                        <p id="linksLegal">Privacy Policy</p>
-                    </a>
+                <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
+                    <a href="" class="text-left disc">Disclaimer</a></ul>
                 </div>
 
-                <div class="nav-item">
-                    <a href="#" class="p-2 nav-link">
-                        <p id="linksLegal">Informed Consent of Service Performed</p>
-                    </a>
+                <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
+                     <a href="" class="text-left disc">Terms of Use</a>
                 </div>
 
-                <div class="nav-item">
-                    <a href="#" class="p-2 nav-link">
-                        <p id="linksLegal">Disclaimer</p>
-                    </a>
+                <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
+                    <a href="" class="text-left disc">Privacy Policy</a>
                 </div>
 
-            </div>
+                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                    <a href="" class="text-left disc">Informed Consent of Service Performed</a>
+                </div>
+
         </div>
-        <div class="col-md-4">
-            <p class="text-left poweredBy">Powered by <img src="/images/lexmeetLogo.png"  alt="lexlogo" height="13" width="85"></p>
-        </div>
+
     </div>
-</div>
-<!-- Footer -->
+
+</footer>
+
