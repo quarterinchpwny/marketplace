1aaf8494ae00f79b5ba23a69ac9edea9d3f14222
Gavin Dane Santos <danesnts@gmail.com>
2021-09-16 18:36:20 +0800

    update

--- public/js/app.js
+++ public/js/app.js
@@ -38134,6 +38134,19 @@ var __WEBPACK_AMD_DEFINE_RESULT__;/**
 
 /***/ }),
 
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
 /***/ "./resources/sass/masculine.scss":
 /*!***************************************!*\
   !*** ./resources/sass/masculine.scss ***!
@@ -41095,7 +41108,8 @@ process.umask = function() { return 0; };
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 		var installedChunks = {
 /******/ 			"/js/app": 0,
-/******/ 			"css/masculine": 0
+/******/ 			"css/masculine": 0,
+/******/ 			"css/app": 0
 /******/ 		};
 /******/ 		
 /******/ 		// no chunk on demand loading
@@ -41145,8 +41159,9 @@ process.umask = function() { return 0; };
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/masculine"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/masculine"], () => (__webpack_require__("./resources/sass/masculine.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/masculine","css/app"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["css/masculine","css/app"], () => (__webpack_require__("./resources/sass/app.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/masculine","css/app"], () => (__webpack_require__("./resources/sass/masculine.scss")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
--- resources/views/layouts/masculineTemplate.blade.php
+++ resources/views/layouts/masculineTemplate.blade.php
@@ -2,21 +2,25 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>LexWeb-Masculine</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <!-- Fonts -->
 <link rel="dns-prefetch" href="//fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@100;400&family=Poppins:wght@200;600&family=Roboto&display=swap" rel="stylesheet">
-<!-- Header and Navbar -->
+
+  <!-- Header and Navbar -->
 <div class="container-fluid" id="topContent">
       <a href="/Masculine">
         <img src="/images/masculineImages/rizalLogo.png" alt="Rizal Logo" id="headerLogo"></a>
     </div>
 @include('layouts.navM', ['from' => 'app'])
+
 <!-- Styles -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <link href="{{ asset('css/masculine.css') }}" rel="stylesheet">
+
 <!-- Scripts -->
 <script >
  function showModal(){
